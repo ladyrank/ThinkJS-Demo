1f768ce0a44ca9c4cd1a7e7a178efa323a84686b
aisinvon@gmail.com <aisinvon@gmail.com>
2015-02-04 11:22:25 +0800

    更新config

--- App/Conf/config.js
+++ App/Conf/config.js
@@ -1,14 +1,13 @@
 module.exports = {
-  //配置项: 配置值
-  port: 8360, //监听的端口
-  db_type: 'mysql', // 数据库类型
-  db_host: '127.0.0.1', // 服务器地址
-  db_port: '', // 端口
-  db_name: 'test', // 数据库名
-  db_user: 'root', // 用户名
-  db_pwd: 'root', // 密码
-  db_prefix: 'think_', // 数据库表前缀
-  //db_prefix: 'wp_test_', // 数据库表前缀
-  app_group_list: ['Home', 'Admin'], //分组列表
-  url_resource_reg: /^(upload\/|resource\/|static\/|favicon\.ico|robot\.txt)/ //判断是否是静态资源的正则
+    //配置项: 配置值
+    port: 8360, //监听的端口
+    db_type: 'mysql', // 数据库类型
+    db_host: '127.0.0.1', // 服务器地址
+    db_port: '', // 端口
+    db_name: 'test', // 数据库名
+    db_user: 'root', // 用户名
+    db_pwd: 'root', // 密码
+    db_prefix: 'think_', // 数据库表前缀
+    app_group_list: ['Home', 'Admin'], //分组列表
+    url_resource_reg: /^(upload\/|resource\/|static\/|favicon\.ico|robot\.txt)/ //判断是否是静态资源的正则
 };
